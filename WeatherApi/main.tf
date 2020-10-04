316fcd1553e321b899a33d843e65d971d844ac7f
Chad Kruszynsky <ckruszynsky@gmail.com>
2020-10-04 09:22:18 -0500

    added terraform configuration

---
+++ WeatherApi/main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+  version = "2.5.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "terraform_test" {
+  name     = "tf_resource_grp"
+  location = "South Central US"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                = "tf_weatherapi"
+  location            = azurerm_resource_group.terraform_test.location
+  resource_group_name = azurerm_resource_group.terraform_test.name
+  ip_address_type     = "public"
+  dns_name_label      = "ckruszynskyweatherapitf"
+  os_type             = "Linux"
+
+  container {
+    name   = "weatherapi"
+    image  = "ckruszynsky/weatherapi"
+    cpu    = "1"
+    memory = "1"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
+
+
