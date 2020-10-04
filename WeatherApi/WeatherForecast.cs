0fffe128a41a3c3ce9264eeead44f3577e7643d3
Chad Kruszynsky <ckruszynsky@gmail.com>
2020-10-04 09:25:35 -0500

    add const for conversion

--- WeatherApi/WeatherForecast.cs
+++ WeatherApi/WeatherForecast.cs
@@ -1,14 +1,13 @@
 using System;
 
-namespace WeatherApi
-{
-    public class WeatherForecast
-    {
+namespace WeatherApi {
+    public class WeatherForecast {
+        private const int FConversion = 32;
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => FConversion + (int) (TemperatureC / 0.5556);
 
         public string Summary { get; set; }
     }
